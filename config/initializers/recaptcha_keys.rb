84ee3e41f4150dc684fba4c51e70e71470537160
Clara Raubertas <clara@parallactic.com>
2021-03-17 13:17:40 -0500

    recaptcha

--- config/initializers/recaptcha_keys.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-Recaptcha.configure do |config|
-  config.site_key = Rails.application.credentials[:recaptcha][:site_key]
-  config.secret_key = Rails.application.credentials[:recaptcha][:site_key]
-end
