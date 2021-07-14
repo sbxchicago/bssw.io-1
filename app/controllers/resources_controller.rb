e33f4878bdc4216857263e51be0c76f43ce68284
Clara Raubertas <clara@parallactic.com>
2021-07-14 12:10:26 -0500

    alpha

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -37,7 +37,7 @@ class ResourcesController < ApplicationController
   end
 
   def authors
-    @authors = Author.displayed.order('alphabetical_name ASC')
+    @authors = Author.displayed.order('alphabetized_name ASC')
     @page = Page.displayed.find_by_name('Contributors')
   end
 
