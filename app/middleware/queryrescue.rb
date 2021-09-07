8f8d4c5014d994fc05f53df9feca780abd25bf04
Clara Raubertas <clara@parallactic.com>
2021-09-07 13:11:13 -0500

    gem

--- app/middleware/queryrescue.rb
+++ app/middleware/queryrescue.rb
@@ -6,7 +6,7 @@ class RefuseInvalidRequest
   def call(env)
     query = Rack::Utils.parse_nested_query(env['QUERY_STRING'].to_s) rescue :bad_query
     if query == :bad_query
-      env['application_controller.instance'].class.action(:render_error).call(env)
+      ApplicationController.action(:render_error).call(env)
     else
       @app.call(env)
     end
