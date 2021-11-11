683839167cc5d1d43e3ed5b9669533a6253dade1
Clara Raubertas <clara@parallactic.com>
2021-11-11 11:20:09 -0600

    date display

--- app/site/event.rb
+++ app/site/event.rb
@@ -27,7 +27,7 @@ class Event < Searchable
 
   def next_date
     array = [['Dates', self.start_at, self.end_at]] + additional_dates.map { |d| [d.label, d.start_at, d.end_at] }
-#    array.delete_if { |tuple| tuple.last && tuple.last < Date.today }
+    array.delete_if { |tuple| tuple[1].nil? }
     array = array.sort_by { |tup| tup[1] || tup[2] }
     array.first
   end
