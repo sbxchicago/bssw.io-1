03b6de769652a55d6704347ea2d6f7a39022f2a3
Clara Raubertas <clara@parallactic.com>
2021-11-16 11:21:04 -0600

    continuing event display

---
+++ app/utilities/additional_date.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class AdditionalDate < ApplicationRecord
+  belongs_to :event
+  has_many :additional_date_values
+
+  include Dateable
+
+  def self.make_date(label_text, dates, event)
+    event.additional_dates.where(label: label_text).each(&:delete) if ['Start Date', 'End Date'].include?(label_text)
+    date = create(
+      label: label_text,
+      event: event
+    )
+    dates.split(';').each do |datetime|
+      date.additional_date_values << AdditionalDateValue.create(date: Chronic.parse(datetime).try(:to_date))
+    end
+  end
+end
---
+++ app/utilities/additional_date_value.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class AdditionalDateValue < ApplicationRecord
+  belongs_to :additional_date
+  delegate :event, to: :additional_date
+
+  scope :from_events, lambda { |events|
+                        joins(additional_date: 'event').where('additional_dates.label != ?', 'End Date').where('site_items.id in (?)', events.map(&:id))
+                      }
+end
