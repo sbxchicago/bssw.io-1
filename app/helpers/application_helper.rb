7f7c6b44f627156ace961300e6ea21fa9af706a0
Clara Raubertas <clara@parallactic.com>
2021-11-16 12:00:34 -0600

    continuing event display

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -34,7 +34,7 @@ module ApplicationHelper
   def show_dates(event)
     (["<strong>Dates</strong> #{date_range(event.start_at, event.end_at)}".html_safe] +
       event.special_additional_dates.map do |date|
-        "<strong>#{date.label.titleize}</strong> #{date_range(date.start_at, date.end_at)}"
+        "<strong>#{date.label.titleize}</strong> #{date_range(date.date)}"
       end
     ).join('<br />').html_safe
   end
