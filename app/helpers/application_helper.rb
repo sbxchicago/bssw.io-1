f8ea07126454698a2974089a4a2a729f4b2ef834
Clara Raubertas <clara@parallactic.com>
2021-12-03 13:42:11 -0600

    event display

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -44,10 +44,10 @@ module ApplicationHelper
   def show_date(date_value)
     date = date_value.additional_date
     if date.label == 'Start Date'
-      "<strong>Dates</strong> #{date_range(date.event.start_at,
+      "#{date_range(date.event.start_at,
                                            date.event.end_at)}".html_safe
     else
-      "<strong>#{date.label.titleize}</strong> #{date_range(date_value.date, nil)}".html_safe
+      "#{date_range(date_value.date, nil)}".html_safe
 
     end
   end
