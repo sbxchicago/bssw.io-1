4bf7a1b0d8953ef4b329b2a7e3ecd73ce5666640
Clara Raubertas <clara@parallactic.com>
2021-12-09 11:20:49 -0600

    .

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -37,11 +37,11 @@ module ApplicationHelper
       #    date_range(adv.date, nil)
         # }
         date.additional_date_values.map{|adv| date_range(adv.date, nil) }.join("; ")
-    }.join('<br />')
+    } #.join('<br />').html_safe
 
-#     ([event.start_at.blank? ? "" : "<strong>Dates</strong> #{date_range(event.start_at, event.end_at)}".html_safe]
-# #     + additionals
-#     ).delete_if?{|d| d.blank? }.join('<br />').html_safe
+     ([event.start_at.blank? ? "" : "<strong>Dates</strong> #{date_range(event.start_at, event.end_at)}".html_safe]
+     + additionals
+     ).delete_if?{|d| d.blank? }.join('<br />').html_safe
   end
 
   def show_date(date_value)
