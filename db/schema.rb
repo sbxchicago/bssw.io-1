f4f634d0524b0c922c6a00a24f34f130b5b2a136
Clara Raubertas <clara@parallactic.com>
2021-12-20 14:00:24 -0600

    search result style

--- db/schema.rb
+++ db/schema.rb
@@ -27,9 +27,6 @@ ActiveRecord::Schema.define(version: 2021_11_15_154657) do
 
   create_table "additional_dates", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "label"
-    t.string "text"
-    t.datetime "start_at"
-    t.datetime "end_at"
     t.integer "event_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -285,6 +282,7 @@ ActiveRecord::Schema.define(version: 2021_11_15_154657) do
     t.text "files_processed"
     t.text "errors_encountered"
     t.string "content_branch"
+    t.string "commit_hash"
   end
 
   create_table "resources", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
@@ -335,8 +333,6 @@ ActiveRecord::Schema.define(version: 2021_11_15_154657) do
     t.string "location"
     t.string "website"
     t.string "organizers"
-    t.date "end_at"
-    t.date "start_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "version_no"
